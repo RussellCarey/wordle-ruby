6a3f026ad73a155752582825aab377c47b99b38f
RussellCarey <62222824+RussellCarey@users.noreply.github.com>
2022-07-28 10:39:17 +0900

    Works as a class based version, needs refactoring

--- game.rb
+++ game.rb
@@ -7,23 +7,41 @@ class Game
     def initialize(word_controller)
       @word_controller = word_controller
       @row_status = []
+      @is_first_round = true;
     end
 
     def start_round
-
         # On new recursion, check if we have won
-        puts("You have won!!!") if win_condition()?
+       
+        if win_condition?()
+            puts("You have won!!!") 
+        end
         
-        puts("TYPE A 5 LETTER WORD IN CAPS")
-        puts("")
+        if @is_first_round 
+            puts(" ")
+            puts("Guess the chosen word! Type in a 5 letter word")
+            puts("Key: c = its in the correct place, x = its not in the word and m = its in the word but a different place.")
+            puts("  ")
+        end
+
+        if !@is_first_round
+            puts(" ")
+            puts("Please try again! Enter another word")
+        end
 
         @user_guess = get_user_guess()
         @row_status = @word_controller.check_word_for_letter(@user_guess)
 
+        puts(" ")
+        puts("Let's check how right you are?")
+        puts(@row_status.join("  "))
+        puts(" ")
+
+        @is_first_round = false
         start_round()
     end
 
-    def get_user_guess
+    private def get_user_guess
         # Get user input
         @guess = gets.chomp
         @guess = @guess.slice(0, 5)
@@ -34,10 +52,10 @@ class Game
         return @guess
     end
 
-    def win_condition?
-        @outcome_string = @round_outcome.join
+    private def win_condition?
+        @outcome_string = @row_status.join
         @count = @outcome_string.count("c")
-        @has_won = @count == 5 ? true : false;
+        @has_won = @count == 5 ? true : false
     end
 
     def reset_game
@@ -46,5 +64,5 @@ class Game
 end
 
 @word_con = Word.new('words.txt')
-@test = Game.new(@word_con)
-@test.start_round()
+@game = Game.new(@word_con)
+@game.start_round()
--- word.rb
+++ word.rb
@@ -4,37 +4,41 @@ class Word
 
     def initialize(textFile)
         @text_array =  File.read(textFile).split
-        @word = nil
+        @word = choose_random_word()
     end
 
     def choose_random_word
         @random_choice = rand(@text_array.length - 1).floor
-        @word = @text_array[@random_choice]
+        @word = @text_array[@random_choice].upcase
+        puts @word
+        
+        return @word
     end
 
     def check_word_for_letter(guess)
         @row_status = []
-        @word_array = @word.split(//);
-        @guess_array = guess.split(//);
+        @word_array = @word.split("");
+        @guess_array = guess.split("");
 
-        @guess_split.each_with_index { |cl, i|
-
-            # Go through each and check if its in the word or not.
-            @word_array.each_with_index { |l, i| 
-                if (l == @guess[index]) 
-                    @row_status[index] = "m"
+        @guess_array.each_with_index {|cl, i|
+            # Does the letter exist in the word?
+            # Can oop and overwrite letter to save lines
+            @word_array.each { |l| 
+                if (l == @guess_array[i]) 
+                    @row_status[i] = "m"
                     break 
                 else
-                    @row_status[index] = "x"
+                    @row_status[i] = "x"
                 end
-
-                # Is the letter correct place, if so overwrite
-                if (@guess[index] == @word[index]) 
-                    @row_status[index] = "c"
-                end;
             }
+
+            # Is the letter correct place?
+            if (@guess_array[i] == @word_array[i]) 
+                @row_status[i] = "c"
+            end;
         }
 
         return @row_status
     end
+
 end
